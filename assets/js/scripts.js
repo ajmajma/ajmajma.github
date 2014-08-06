29725a9b6d5f2c940717365a4b55357d01e5344a
ajmajma <alexmoser@macintosh.home>
2014-08-05 20:23:25 -0400

    a
    a

---
+++ assets/js/scripts.js
@@ -0,0 +1,26 @@
+    window.addEventListener("scroll", function() {
+    if (window.scrollY > 500) {
+        $('.mobile-nav-header').addClass("removeBg");
+       $('.mobile-nav-top').find(".navbar-brand").slideUp();
+    }
+    else {
+        $('.mobile-nav-header').removeClass("removeBg");
+         $('.mobile-nav-top').find(".navbar-brand").slideDown();
+    }
+},false); 
+
+$('.hamburg').on('click touchend', function(e) {
+  e.stopImmediatePropagation();
+            e.preventDefault();
+  
+  if($(this).hasClass("cross")){
+ $(this).removeClass ("cross");
+    $(".mobile-nav-ul").slideUp();
+
+  }else{
+    $(this).addClass("cross");
+    $(".mobile-nav-ul").slideDown();
+  }
+  
+
+});
